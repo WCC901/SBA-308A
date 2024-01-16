91a2d7e13c1810c1ac679a7b1dc269d0837fb785
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2024-01-15 19:43:03 -0500

    Added needed functions to DnD.js that will be exported later

--- DnD.js
+++ DnD.js
@@ -1,2 +1,29 @@
+async function fetchARandomClass() {
+    try {
+        const response = await fetch("https://api.open5e.com/classes/?cr=3");
+        const classData = await response.json();
+        const classList = await classData.results;
+        return classList[Math.round(Math.random() * ((classList.length) - (-0.49)) + (-0.49))];
+    } catch (error) {
+        alert("Error in the response from API: " + error)
+    }
+}
 
+async function fetchARandomRace() {
+    const response = await fetch("https://api.open5e.com/races/?cr=3");
+    const raceData = await response.json();
+    const raceList = await raceData.results;
+    console.log(raceList[Math.round(Math.random() * ((raceList.length) - (-0.49)) + (-0.49))]);
+}
 
+function rollStats() {
+    const stats = {
+        Strength: Math.round((Math.random() * (6 - 0) + 0) * 4),
+        Dexterity: Math.round((Math.random() * (6 - 0) + 0) * 4),
+        Constitution: Math.round((Math.random() * (6 - 0) + 0) * 4),
+        Intelligence: Math.round((Math.random() * (6 - 0) + 0) * 4),
+        Wisdom: Math.round((Math.random() * (6 - 0) + 0) * 4),
+        Charisma: Math.round((Math.random() * (6 - 0) + 0) * 4),
+    }
+    return stats;
+}

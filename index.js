3da78fba3ad22d7e16f422547f8eb3f7c140c27e
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2024-01-21 00:52:07 -0500

    Added extra logic to the recruit button so it replaces the older pictures.

--- index.js
+++ index.js
@@ -3,75 +3,30 @@ import * as DnD from "./DnD.js";
 const teamGallery = document.getElementById("teamGallery");
 const recruitButton = document.getElementById("recruitButton");
 const characterArea = document.getElementById("characters");
-const promiseObj = DnD.fetchARandomRace();
-
-// promiseObj.then(race => {
-//     console.log(race.name);
-// });
-// console.log(DnD.rollStats());
-
-// function getURL(raceName) {
-//     switch (raceName) {
-//         case "Dwarf":
-//             return "dwarf.jpg";
-//         case "Elf":
-//             return "elf.jpg";
-//         case "Halfling":
-//             return "halfling.jpg";
-//         case "Human":
-//             return "human.jpg";
-//         case "Dragonborn":
-//             return "dragonborn.jpg";
-//         case "Gnome":
-//             return "gnome.jpg";
-//         case "Half-Elf":
-//             return "halfElf.jpg";
-//         case "Half-Orc":
-//             return "halfOrc.jpg";
-//         case "Tiefling":
-//             return "tiefling.jpg";
-//         case "Alseid":
-//             return "alseid.jpg";
-//         case "Catfolk":
-//             return "catfolk.jpg";
-//         case "Darakhul":
-//             return "darakhul.jpg";
-//         case "Derro":
-//             return "derro.jpg";
-//         case "Drow":
-//             return "drow.jpg";
-//         case "Erina":
-//             return "erina.jpg";
-//         case "Gearforged":
-//             return "gearforged.jpg";
-//         case "Minotaur":
-//             return "minotaur.jpg";
-//         case "Mushroomfolk":
-//             return "mushroom.png";
-//         case "Satarre":
-//             return "satarre.jpg";
-//         case "Shade":
-//             return "shade.jpg";
-//     }
-// }
-
+const recruitOptions = document.querySelectorAll("#characterPic");
+console.log(DnD.rollStats());
 
 function handleButtonClick() {
-    for (let i = 0; i <= 2; i++) {
+    for (let i = 0; i <= 3; i++) {
         const race = DnD.fetchARandomRace();
-        console.log(race);
         const characterPic = document.createElement("img");
         race.then(result => {
             characterPic.setAttribute("src", `${result.name.toLowerCase()}.jpg`);
+            characterPic.setAttribute("id", "characterPic");
             characterPic.setAttribute("width", "290px");
-            characterArea.appendChild(characterPic);
-            console.log(characterPic);
+            if (characterArea.childElementCount == 3) {
+                for (let i = 0; i <= characterArea.childElementCount; i++) {
+                    const oldCharacter = characterArea.children[i];
+                    console.log(oldCharacter);
+                    characterArea.replaceChild(characterPic, oldCharacter);
+                }
+            } else {
+                characterArea.appendChild(characterPic);
+            }
         });
-        // characterPic.setAttribute("width", "400px")
-        // characterArea.appendChild(characterPic);
-        // console.log(characterPic);
     }
 }
 
+console.log(recruitOptions);
 
 recruitButton.addEventListener("click", handleButtonClick);
